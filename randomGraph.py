43efb92089d91f771af3cbb9632e1d537c405179
AlexeyLepov <77492646+AlexeyLepov@users.noreply.github.com>
2022-11-26 01:06:38 +0300

    Update randomGraph.py

--- randomGraph.py
+++ randomGraph.py
@@ -3,7 +3,7 @@ import networkx as nx
 
 
 # create a directed multi-graph
-multiplier = 50
+multiplier = 15
 minVerCount = 5*multiplier
 minColoredCount = 1*multiplier
 
@@ -14,7 +14,7 @@ colors = ['r','b','y','c','g']*minColoredCount
 
 
 plt.figure(figsize=(20,10)) # plotting a frame
-nx.draw(G, connectionstyle=f'arc3, rad = {rad}', with_labels = False, node_size=50, node_color=colors) # plotting the graph
+nx.draw(G, connectionstyle=f'arc3, rad = {rad}', with_labels = False, node_size=100, node_color=colors) # plotting the graph
 plt.show()  # pause before exiting
 
 
